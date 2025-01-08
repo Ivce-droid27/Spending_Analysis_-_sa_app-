1cf54b39f1e8b5bf1c86d5909e36dd0b97d95c62
Ivce-droid27 <ivopendarovski005@gmail.com>
2025-01-08 22:14:16 +0100

    Update user_spent.py
    1. Во овој file е додадено class Average_spending_age со потребниот ифнормациски код за ваа класа.
    2. Овој код што е  #autoincrement=True е додадено од ChatGPT поради id дека него читаше претходно во мала измена во кодот после уште мала измена може без вој код во линијата да фукционира

--- user_spent.py
+++ user_spent.py
@@ -3,7 +3,7 @@ from con_sqlalchemy import db
 class Total(db.Model):
     __tablename__ = "total_spent"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True) # autoincrement=True
     total_spent = db.Column(db.Float, nullable=False)
     year = db.Column(db.Float, nullable=False)
     user_id = db.Column(db.Integer,  db.ForeignKey('users_info.id'), nullable=False)
@@ -18,11 +18,25 @@ class Total(db.Model):
         }
 
 
-# class Average_spending_age(db.Model):
-#     __tablename__ = "average_spending_by_age"
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     average_spent = db.Column(db.Float, nullable=False)
-#     total_id = db.Column(db.Integer,  db.ForeignKey('total_spent.id'), nullable=False)
+class Average_spending_age(db.Model):
+    __tablename__ = "average_spending_by_age"
 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users_info.id'), nullable=False)
+    name = db.Column(db.String(20), db.ForeignKey('users_info.id'), nullable=False)
+    age = db.Column(db.Integer, db.ForeignKey('users_info.id'), nullable=False)
+    pay_check = db.Column(db.Integer, db.ForeignKey('users_info.id'), nullable=False, default=0)
+    total_spent = db.Column(db.Float, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    average_spent = db.Column(db.Float, nullable=False)
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'name': self.name,
+            'age': self.age,
+            'total_spent': self.total_spent,
+            'year': self.year,
+            'average_spent': self.average_spent
+        }
