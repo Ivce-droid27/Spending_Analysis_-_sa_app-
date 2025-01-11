326eb633aaacfe30945021e5fd2921e157a44623
Ivce-droid27 <ivopendarovski005@gmail.com>
2025-01-11 02:19:21 +0100

    Update user_spent.py

--- user_spent.py
+++ user_spent.py
@@ -3,18 +3,19 @@ from con_sqlalchemy import db
 class Total(db.Model):
     __tablename__ = "total_spent"
 
-    id = db.Column(db.Integer, primary_key=True) # autoincrement=True
-    total_spent = db.Column(db.Float, nullable=False)
-    year = db.Column(db.Float, nullable=False)
+    id = db.Column(db.Integer, primary_key=True) # , autoincrement=True - slucajno ako treba
     user_id = db.Column(db.Integer,  db.ForeignKey('users_info.id'), nullable=False)
-
+    name = db.Column(db.String(20),  db.ForeignKey('users_info.name'), nullable=False)
+    total_spent = db.Column(db.Float, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
 
     def to_dict(self):
         return {
             'id': self.id,
+            'user_id': self.user_id,
+            'name': self.name,
             'total_spent': self.total_spent,
-            'year': self.year,
-            'user_id': self.user_id
+            'year': self.year
         }
 
 
