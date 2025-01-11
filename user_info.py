6161cda96964447c8ea413b52b0bbba95cda608b
Ivce-droid27 <ivopendarovski005@gmail.com>
2025-01-11 02:18:47 +0100

    Update user_info.py

--- user_info.py
+++ user_info.py
@@ -3,11 +3,11 @@ from con_sqlalchemy import db
 class User_info(db.Model):
     __tablename__ = "users_info"
 
-    id = db.Column(db.Integer, primary_key=True) # smeneto e od user_id vo id
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(65), nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    pay_check = db.Column(db.Integer, nullable=False, default=0) # premeste no e na posledno mestp
+    pay_check = db.Column(db.Integer, nullable=False, default=0)
 
     def to_dict(self):
         return {
