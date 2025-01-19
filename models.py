463ea2d986f9880d3c1213880052ee7f22c15531
Ivce-droid27 <ivopendarovski005@gmail.com>
2025-01-19 18:57:04 +0100

    Update and rename user_info.py to models.py
    The file user_spent is delete.
    The file user_info is change to models and the class Total was moved here.

---
+++ models.py
@@ -0,0 +1,38 @@
+from con_sqlalchemy import db
+
+class User_info(db.Model):
+    __tablename__ = "users_info"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(65), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    pay_check = db.Column(db.Integer, nullable=False, default=0)
+
+    def to_dict(self):
+        return {
+            'user_id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'age': self.age,
+            'pay_check': self.pay_check
+        }
+
+
+class Total(db.Model):
+    __tablename__ = "total_spent"
+
+    id = db.Column(db.Integer, primary_key=True) # , autoincrement=True - slucajno ako treba
+    user_id = db.Column(db.Integer,  db.ForeignKey('users_info.id'), nullable=False)
+    # user_name = db.Column(db.String(20), db.ForeignKey('users_info.name'), nullable=False)
+    total_spent = db.Column(db.Float, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            # 'name': self.user_name,
+            'total_spent': self.total_spent,
+            'year': self.year
+        }
