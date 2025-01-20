cb9602d65247f0e66b3a5ae78ca188c744bf8719
Ivce-droid27 <ivopendarovski005@gmail.com>
2025-01-20 02:35:49 +0100

    Update con_sqlalchemy.py
    Here we add the why to the test collection on MongoDB before adding the real collection

--- con_sqlalchemy.py
+++ con_sqlalchemy.py
@@ -1,3 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+from pymongo.mongo_client import MongoClient
+
 
 db = SQLAlchemy()
+
+
+mongo_client = MongoClient("mongodb+srv://admin:admin@cluster0.kuu7e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+test_code_db = mongo_client['users_vouchers_2'] ## MongoDB database
+test_code_collection = test_code_db[('test_collenctiones')] ## MongoDB test collection for app collecton
