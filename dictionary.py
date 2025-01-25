e8626ef56da9b928825c1d73ab7f7c081beb2e21
Ivce-droid27 <ivopendarovski005@gmail.com>
2025-01-25 01:09:23 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -8,45 +8,34 @@ BOT_USERNAME: Final = '@dift_voucherBot'
 
 ## COMMANDS
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # await update.message.reply_text("Hello! I am here to help you who much you have spent so you can "
-    #                                 f"get or to know the nolege for how to get a voucher.")
     await update.message.reply_text("Hello! I am Voucher Bot and I am at your service.")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! I am a Voucher Bot. Please type what is your problem. So I can help you.")
 
+async def end_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Good buy and have a nice day Sir/Mam.")
 
-## RESPONSES
-def handle_responses(text: str):
+## HELP RESPONSES
+def help_responses(text: str):
     processed: str = text.lower()
 
     if 'hello' in processed:
         return "Hello there!"
 
-    if 'i have a problem' in processed:
-        return "Hello! Sorry that you have a prble. How can I help you?"
+    if 'can you help me with the voucher?' in processed:
+        return "Yes of course.What is your problem?"
 
-    return "I do not understand what you wrote..."
-
-## HELP RESPONSES
-def help_responses(text: str):
-    processd: str = text.capitalize()
-
-    help1 = "How much for to win the voucher?"
-    help2 = "Can you help me to find how much did I spent?"
-    help3 = "How long does voucher last? After winning "
-
-    if help1 in processd:
+    if 'how much for to win the voucher?' in processed:
         return "You need to spent 1000 or more to able to win the voucher"
 
-    if help2 in processd:
-        return "No problem.That's a job."
+    if 'can you help me to find how much did i spent?' in processed:
+        return "No problem.That's my job."
 
-    if help3 in processd:
+    if ('how long does voucher last? after winning?') in processed:
         return "I don't have an answer to that question."
-    
-    return "I do not understand what you wrote..."
 
+    return "I do not understand what you wrote..."
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -58,11 +47,11 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if massage_type == 'group':
         if BOT_USERNAME in text:
             new_text: str = text.replace(BOT_USERNAME, '').strip()
-            response: str = handle_responses(new_text)
+            response: str = help_responses(new_text)
         else:
             return
     else:
-        response: str = handle_responses(text)
+        response: str = help_responses(text)
 
     print('Bot:', response)
     await update.message.reply_text(response)
@@ -78,6 +67,7 @@ if __name__ == '__main__':
     ## COMMANDS
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('end', end_command))
 
     ## MESSAGES
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
